158fe7a652fd0fae370986149770bf79ea7a4dc3
Manoj P <thedarkstrix@gmail.com>
2019-08-04 17:10:41 +0530

    Add packages
    ** Add msql
    ** Add db connection auth
    ** Add mysql2
    ** Make a demo model User

--- server/main.js
+++ server/main.js
@@ -1,9 +1,13 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
-server.listen();
+
+
+app.listen(6969, err => {
+    if (err) throw err;
+    console.log(`> Server running on http://localhost:6969`);
+});
